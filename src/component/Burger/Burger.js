7305d04b77e264bca277e39235c46d4bac1699c7
Babiy7 <olegbabiy77@gmail.com>
2019-10-23 18:30:53 +0300

    Made confirm ingredients.

--- src/component/Burger/Burger.js
+++ src/component/Burger/Burger.js
@@ -3,15 +3,20 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = props => {
-  const transformIngredients = Object.keys(props.ingredients).map(
-    ingredientKey => {
+  let transformIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey => {
       return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
         return (
           <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
         );
       });
-    }
-  );
+    })
+    .reduce((array, el) => {
+      return array.concat(el);
+    }, []);
+  if (transformIngredients.length === 0) {
+    transformIngredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,9 +4,10 @@ import Burger from "../../component/Burger/Burger";
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1
+      salad: 0,
+      meat: 0,
+      cheese: 0,
+      bacon: 0
     }
   };
 
