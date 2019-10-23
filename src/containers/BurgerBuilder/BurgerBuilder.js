4c986783995297db79c147a101dfcb30373f4536
Babiy7 <olegbabiy77@gmail.com>
2019-10-23 17:29:40 +0300

    Created the transformedIngredients method.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import Burger from "../../component/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      meat: 1,
+      cheese: 1
+    }
+  };
+
   render() {
     return (
       <>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build controls</div>
       </>
     );
