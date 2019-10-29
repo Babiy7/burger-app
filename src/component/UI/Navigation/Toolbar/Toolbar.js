5e2c4d910ef043beeeb84bf9e0a3845d333942f5
Babiy7 <olegbabiy77@gmail.com>
2019-10-29 13:15:39 +0200

    Added the Toolbar into project.

---
+++ src/component/UI/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Toolbar.module.css";
+
+const Toolbar = props => {
+  return (
+    <div className={classes.Toolbar}>
+      <div>Menu</div>
+      <div>logo</div>
+      <ul>Menu</ul>
+    </div>
+  );
+};
+
+export default Toolbar;
