ce99c05ba3cb8fea169f89a841cbaca155f3498c
Babiy7 <olegbabiy77@gmail.com>
2019-10-29 13:16:23 +0200

    Fixed the mobile screen.

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import classes from "./Layout.module.css";
+import Toolbar from "../UI/Navigation/Toolbar/Toolbar";
 
 const Layout = props => {
   return (
     <div className={classes.Layout}>
-      <div className={classes.Toolbar}>Drawer, logo</div>
-      <main className={classes.Content}>{props.children}</main>
+      <Toolbar />
+      <main className={classes.Main}>{props.children}</main>
     </div>
   );
 };
