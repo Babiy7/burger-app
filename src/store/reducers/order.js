b9538aa1f3acf2609b83a5c9604210bdafcdf6e1
Babiy7 <olegbabiy77@gmail.com>
2019-12-06 12:36:55 +0200

    Fix all reducers.

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,44 +1,51 @@
 import * as ActionType from "../actions/actionTypes";
+import { updatedState } from "../../shared/utility";
 
 const innitialState = {
   success: false,
-  error: false,
-  errorMessage: "",
+  error: null,
   orders: [],
   order: {},
   loading: false
 };
 
+const init = (state, action) =>
+  updatedState(state, {
+    orders: state.orders.concat(action.orders),
+    loading: false,
+    error: null
+  });
+
+const success = (state, action) =>
+  updatedState(state, {
+    success: true,
+    order: { id: action.id, order: action.order },
+    loading: false,
+    error: null
+  });
+
+const fail = (state, action) =>
+  updatedState(state, {
+    error: action.error,
+    errorMessage: action.error,
+    loading: false
+  });
+
+const loading = state => updatedState(state, { loading: true });
+
 const ingredientsReducer = (state = innitialState, action) => {
   switch (action.type) {
     case ActionType.INIT_ORDERS: {
-      return {
-        ...state,
-        orders: state.orders.concat(action.orders),
-        loading: false
-      };
+      return init(state, action);
     }
     case ActionType.SUCCESS_ORDER: {
-      return {
-        ...state,
-        success: true,
-        order: { id: action.id, order: action.order },
-        loading: false
-      };
+      return success(state, action);
     }
     case ActionType.FAIL_ORDER: {
-      return {
-        ...state,
-        error: true,
-        errorMessage: action.error,
-        loading: false
-      };
+      return fail(state, action);
     }
     case ActionType.LOADING_ORDER: {
-      return {
-        ...state,
-        loading: true
-      };
+      return loading(state);
     }
     default: {
       return state;
--- src/store/reducers/orders.js
+++ src/store/reducers/orders.js
@@ -1,31 +1,28 @@
 import * as ActionType from "../actions/actionTypes";
+import { updatedState } from "../../shared/utility";
 
 const innitialState = {
-  error: false,
+  error: null,
   orders: null,
   loading: false
 };
 
+const init = (state, action) =>
+  updatedState(state, { orders: action.orders, error: false });
+
+const loading = state => updatedState(state, { loading: true });
+
+const fail = (state, action) => updatedState(state, { error: action.error });
+
 const ordersReducer = (state = innitialState, action) => {
   switch (action.type) {
-    case ActionType.INIT_ORDERS: {
-      return {
-        ...state,
-        orders: action.orders,
-        error: false
-      };
-    }
+    case ActionType.INIT_ORDERS:
+      return init(state, action);
     case ActionType.LOADING_ORDER: {
-      return {
-        ...state,
-        loading: action.payload
-      };
+      return loading(state);
     }
     case ActionType.FAIL_ORDERS: {
-      return {
-        ...state,
-        error: action.error
-      };
+      return fail(state, action);
     }
     default: {
       return state;
